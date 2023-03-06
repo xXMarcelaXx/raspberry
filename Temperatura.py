0aecfd8d61c0d06beef25f457df56c6b47a8166b
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2023-03-06 13:11:08 -0600

    temp

--- Temperatura.py
+++ Temperatura.py
@@ -4,7 +4,6 @@ class temperatura:
     def __init__(self): 
         super().__init__()
     def temperatura(): 
-        while True:
             sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
             pin = 22 #Pin en la raspberry donde conectamos el sensor
             humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
